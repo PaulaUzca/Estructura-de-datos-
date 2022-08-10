698ad5da2eccb633da626ec8a46de924fbc68da2
PaulaUzca <paula.uzcategui68@gmail.com>
2022-08-10 09:39:56 -0500

    Reto 2 terminado

--- Retos/Reto2/Reto2.cpp
+++ Retos/Reto2/Reto2.cpp
@@ -1,20 +1,56 @@
-// Reto2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+using namespace std;
 
+
+void printArray(int array[4]);
+/*
+*  Dado un array de enteros no ordenados, verifique si contiene una tupla de cuatro elementos(cuarteto) que tengan una suma dada.
+*/
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n, e;
+    cout << "Ingrese el numero de elementos del arreglo: ";
+    cin >> n;
+    vector<int> array;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Elemento [" << i << "]: ";
+        cin >> e;
+        array.push_back(e);
+    }
+
+    vector<int[4]> cuartetos;
+    int suma;
+    cout << "Ingrese una suma: ";
+    cin >> suma;
+    int cuarteto[4];
+    // combinatoria
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+        cuarteto[0] = array[i];
+        for (int j = i + 1; j < n; j++) {
+            cuarteto[1] = array[j];
+            total = cuarteto[0] + cuarteto[1];
+            for (int k = j + 1; k < n; k++) {
+                cuarteto[2] = array[k];
+                total = cuarteto[0] + cuarteto[1]+ cuarteto[2];
+                for (int y = k + 1; y < n; y++) {
+                    cuarteto[3] = array[y];
+                    total = cuarteto[0] + cuarteto[1] + cuarteto[2] + cuarteto[3];
+                    if (total == suma) {
+                        printArray(cuarteto);
+                    }
+                }
+            }
+        }
+    }
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void printArray(int array[4]) {
+    for (int i = 0; i < 4; i++) {
+        cout << array[i] << ", ";
+    }
+    cout << "\n";
+}
