6483c2095ee3d577b49725b67ec1454da2fa8f20
PaulaUzca <paula.uzcategui68@gmail.com>
2022-08-29 10:01:55 -0500

    Taller Arbol 1

---
+++ Arbol/Arbol.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+private:
+	int element;
+	vector<TreeNode*> children;
+public:
+	TreeNode(int element) {
+		this->element = element;
+	}
+	void addChild(TreeNode* child) {
+		children.push_back(child);
+	}
+	void fillTree(TreeNode* nodo) {
+		int hijos, e;
+		cout << "Cuantos hijos tiene el nodo: ";
+		cin >> hijos;
+		for (int i = 0; i < hijos; i++) {
+			cout << "Hijo " << i + 1 << ":";
+			cin >> e;
+			TreeNode* child = new TreeNode(e);
+			cout << "\t";
+			fillTree(child);
+			nodo->addChild(child);
+		}
+	}
+	void printNodo() {
+		cout << " [" << element <<"] ";
+	}
+	void printTree(TreeNode* nodo) {
+		nodo->printNodo();
+		if (nodo->children.size() > 0) {
+			cout << "{";
+			for (int i = 0; i < nodo->children.size(); i++) {
+				printTree(nodo->children[i]);
+			}
+			cout << "}";
+		}
+	}
+};
+
+// Llenar un árbol desde 0
+int main()
+{
+	int raiz, hijos, e;
+	cout << "Ingrese el valor de la raiz del arbol: ";
+	cin >> raiz;
+	TreeNode* nodoRaiz = new TreeNode(raiz);
+	nodoRaiz->fillTree(nodoRaiz);
+	nodoRaiz->printTree(nodoRaiz);
+}
